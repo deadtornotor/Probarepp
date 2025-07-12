3e0db846d6eabd917d3d096f522465bbee6a6180
deadtornotor <deadtornotor@gmail.com>
2025-07-12 22:41:52 +0200

    Added parsed and failde validation

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,4 +1,36 @@
-#include <probare_entry>
+#include "probare_core/data.hpp"
+#include <iostream>
+#include <ostream>
+#include <probare>
+
+int main()
+{
+    // Start time
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    int error_value = probare::run_tests(true);
+
+    // End time
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    // Duration in milliseconds
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+        end_time - start_time);
+
+    std::cout << "Duration: " << duration.count() << " ms" << std::endl;
+
+    if (int failed = probare::testing.failed.load() != 0) {
+        std::cout << "[ERROR] Had " << failed << " unexpected failing tests"
+                  << std::endl;
+    }
+
+    if (int passed = probare::testing.passed.load() != 775) {
+        std::cout << "[ERROR] Expected 775 tests to pass" << std::endl;
+        std::cout << "[ERROR] Passed test " << passed << std::endl;
+    }
+
+    return error_value;
+}
 
 TEST(self_test_sample)
 {
